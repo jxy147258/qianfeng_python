8c53a7eff17c05ab20a4e796eb8f15d45de8ec63
jxy147258 <1162591945@qq.com>
2020-02-03 17:02:25 +0800

    from pycharm commuity edition 20200203170200

--- FeatureTestingPy/KaoyanNews.py
+++ FeatureTestingPy/KaoyanNews.py
@@ -1,4 +1,4 @@
-from urllib import urlopen
+import urllib.request
 from bs4 import BeautifulSoup
 import re
 
@@ -6,8 +6,8 @@ for page in range(17):
     if page == 0:
         continue
     firstUrl = "http://new.bj.xdf.cn/zhentiku/daxue/kaoyan/kyzyk/list_381_" + str(page) + ".html"
-    print "[Begin] scrap page", firstUrl
-    html = urlopen(firstUrl)
+    print ("[Begin] scrap page", firstUrl)
+    html = urllib.request.urlopen(firstUrl)
     data = html.read()
     bsobj = BeautifulSoup(data)
 
@@ -16,9 +16,9 @@ for page in range(17):
     for l in li:
         url = "http://new.bj.xdf.cn" + l.attrs["href"]
         filename = l.attrs["title"] + ".html"
-        subdata = BeautifulSoup(urlopen(url).read())
+        subdata = BeautifulSoup(urllib.request.urlopen(url).read())
         with open(filename, 'w') as f:
             f.write('<meta charset=\"UTF-8\">\n')
             f.write('%s' % subdata.select(".article-wrap"))
             f.close()
-    print "[End]" 
+    print ("[End]")
--- FeatureTestingPy/shijian_zhicha.py
+++ FeatureTestingPy/shijian_zhicha.py
@@ -1,11 +1,11 @@
 #时间之差
 
 import datetime
-d1 = datetime.datetime(2013, 8, 05,15,50)
+d1 = datetime.datetime(2013, 8, 5,15,50)
 d2 = datetime.datetime(2013, 8, 4,21,9,0,0)
-print u'相差：%s天'%(d1-d2).days
+print (u'相差：%s天'%(d1-d2).days)
 #时区转换，当前系统所在时区+1
 d = datetime.datetime.now()
 d = d + datetime.timedelta(seconds=3600)
-print d
-print d.ctime()
+print (d)
+print (d.ctime())
