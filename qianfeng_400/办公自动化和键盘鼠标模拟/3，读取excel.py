799e28719ca50ce777e965a6066ba1882212f51c
jxy147258 <1162591945@qq.com>
2020-02-14 00:46:05 +0800

    daily update 2020-02-14

--- qianfeng_400/办公自动化和键盘鼠标模拟/3，读取excel.py
+++ qianfeng_400/办公自动化和键盘鼠标模拟/3，读取excel.py
@@ -1,15 +1,27 @@
 #xls
-# openpyxl 处理xlsx格式
+# openpyxl 处理xlsx格式，不能处理xls文件
 
 from openpyxl.reader.excel import load_workbook
 
 
 def readXlsxFile(path):
-    # 得到整张表
+    # 得到整张表对象
     file = load_workbook(filename=path)
-    print(file.get_sheet_names())
-
-
+    # 得到所有sheet名称
+    sheets = file.sheetnames
+    # 得到一个sheet对象
+    sheet0 = file[sheets[0]]
+    # 得到行数、列数、表明
+    # print(sheet0.max_row)
+    # print(sheet0.max_column)
+    print(sheet0.title)
+    # 读取一张表中的数据
+    for rowNum in range(1,sheet0.max_row+1):
+        rowList = []
+        for columnNum in range(1,sheet0.max_column+1):
+            value = sheet0.cell(row = rowNum,column = columnNum).value
+            rowList.append(value)
+        print(rowList)
 
 
 path = r"GeoLite2-ASN-Blocks-IPv4.xlsx"
---
+++ qianfeng_400/办公自动化和键盘鼠标模拟/6、返回整体xlsx文件.py
@@ -0,0 +1,29 @@
+
+from openpyxl.reader.excel import load_workbook
+
+
+def readXlsxFile(path):
+    # 得到整张表对象
+    file = load_workbook(filename=path)
+    # 得到所有sheet名称
+    sheets = file.sheetnames
+    # 整张表数据存在xlsxInfo中
+    # 一张表的数据sheetInfo中
+    # 一行数据存在rowInfo中
+
+    # 这个声明不能写在下面这个循环里面，因为那样的话，每次循环就会把前面的内容清空了，再添加新内容
+    xlsxInfo = {}
+    for i in range(len(sheets)):
+        sheet = file[sheets[i]]
+        sheetInfo = []
+        for rowNum in range(1,sheet.max_row+1):
+            rowInfo = []
+            for columnNum in range(1,sheet.max_column+1):
+                rowInfo.append(sheet.cell(row = rowNum,column = columnNum).value)
+            sheetInfo.append(rowInfo)
+        xlsxInfo[sheet] = sheetInfo
+    print(xlsxInfo)
+
+
+path = r"aa13.xlsx"
+print(readXlsxFile(path))
