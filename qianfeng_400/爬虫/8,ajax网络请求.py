96142747cbf6a3a08697b82e9fe4bba263eb6b26
jxy147258 <147258Jxy>
2020-02-24 01:40:24 +0800

    daily update 2020-02-24#1

--- qianfeng_400/爬虫/8,ajax网络请求.py
+++ qianfeng_400/爬虫/8,ajax网络请求.py
@@ -1,1 +1,27 @@
+import json
+import ssl
+import urllib.request
+import urllib.parse
 
+
+def Crawler(url):
+    # 用url和headers构造req
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0"}
+    req = urllib.request.Request(url, headers=headers)
+    # 用req和context构造response
+    context = ssl._create_unverified_context()
+    response = urllib.request.urlopen(req, context=context)
+    #
+    jsonStr = response.read().decode("utf-8")
+    # jsonData = json.loads(jsonStr)
+    return jsonStr
+
+
+
+'''url = "https://movie.douban.com/j/chart/top_list?type=11&interval_id=100%3A90&action=&start=0&limit=20"
+info = Crawler(url)
+print(info)'''
+for i in range(1,11):
+    url = "https://movie.douban.com/j/chart/top_list?type=11&interval_id=100%3A90&action=&start="+str(i*20)+"&limit=20"
+    info = Crawler(url)
+    print(info)
