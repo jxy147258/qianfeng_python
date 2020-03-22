9fe5f0bcbc748eaaf252c865314932e8c3684e20
jxy147258 <1162591945@qq.com>
2020-03-22 17:14:49 +0800

    daily update 2020-03-22#1

--- qianfeng_400/Django_learning/firstSite/project/myApp/urls.py
+++ qianfeng_400/Django_learning/firstSite/project/myApp/urls.py
@@ -2,5 +2,20 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # url中端口号8000的后面为空匹配此条
     url(r'^$', views.index),
+    # url中端口号的后面是任意的数字匹配此条
+    # url(r"^(\d+)/$", views.detail),
+    # 关于正则的一个技巧，如果要显示100/200/这种样式，
+    url(r"^(\d+)/(\d)/$", views.detail),
+
+    # 接受模板的传递，然后显示页面
+    url(r"^grades/$",views.grades),
+
+    # 接受模板的传递，然后显示页面
+    url(r"^students/$", views.students),
+
+
+    url(r"^grades/(\d+)/$", views.gradesStudents),
+
 ]
--- qianfeng_400/Django_learning/firstSite/project/myApp/views.py
+++ qianfeng_400/Django_learning/firstSite/project/myApp/views.py
@@ -2,8 +2,44 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Grades
+from .models import Students
 
 
-# 定义了一个视图，也就是一个html页面
+# 定义了一个视图，也就是一个html页面,这是不是用模板
+# def index(request):
+#     return HttpResponse("sunck is a good man !")
+# 定义一个视图，使用模板
 def index(request):
-    return HttpResponse("sunck is a good man !")
+    return render(request, "myApp/index.html")
+
+
+def detail(request, num, num2):
+    return HttpResponse("detail-%s-%s" % (num, num2))
+
+
+def grades(request):
+    # 先去模板里取数据
+    gradesList = Grades.objects.all()
+    # 将数据传递给模板，模板再渲染页面，将渲染 好的页面返回给浏览器
+    return render(request, "myApp/grades.html", {"grades": gradesList})
+    # 参数说明，"myApp/grades.html"是定义好的模板页面，
+    # {"grades": gradesList})中的grades是模板中定义的变量，gradesList是视图中的变量
+
+
+def students(request):
+    # 先去模板里取数据
+    studentsList = Students.objects.all()
+    # 将数据传递给模板，模板再渲染页面，将渲染 好的页面返回给浏览器
+    return render(request, "myApp/students.html", {"students": studentsList})
+
+
+def gradesStudents(request, num):
+    # 按传入的班级id号去取所有学生信息
+    grade = Grades.objects.get(pk=num)
+    studentsList = grade.students_set.all()
+    # 返回给模板
+    return render(request, "myApp/students.html", {"students": studentsList})
+
+
+
