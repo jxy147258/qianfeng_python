dc8e4da06e8c309cde63a1933a9ffb2c0a467a19
jxy147258 <1162591945@qq.com>
2020-03-29 23:30:42 +0800

    daily update 2020-03-29#1

--- qianfeng_400/Django_learning/firstSite/project/myApp/urls.py
+++ qianfeng_400/Django_learning/firstSite/project/myApp/urls.py
@@ -3,13 +3,17 @@ from . import views
 
 urlpatterns = [
     # url中端口号8000的后面为空匹配此条
-    url(r'^$', views.index,name="index"),
+    url(r'^$', views.index, name="index"),
+    # url中端口号8000的后面为index.html匹配此条
+    url(r'index.html', views.index),
+
+
     # 为了验证url中/的位置，此条的完整url是，127.0.0.1:8000/a
     url(r'^a/', views.index),
     # url中端口号的后面是任意的数字匹配此条
-    # url(r"^(\d+)/$", views.detail),
+    url(r"^(\d+)/$", views.detail1),
     # 关于正则的一个技巧，如果要显示100/200/这种样式，用两个括号
-    url(r"^(\d+)/(\d)/$", views.detail),
+    url(r"^(\d+)/(\d+)/$", views.detail2),
 
     # 接受模板的传递，然后显示页面
     url(r"^grades/$", views.grades),
@@ -54,5 +58,18 @@ urlpatterns = [
     url(r"^showregist/$", views.showregist),
     url(r"^showregist/regist/$", views.regist),
     url(r"^showresponse/$", views.showresponse),
+
+    # 测试cookie
+    url(r"^cookietest/$", views.cookietest),
+
+    # HttpResponseRedirect
+    url(r"^redirect1/$", views.redirect1),
+    url(r"^redirect2/$", views.redirect2),
+
+
+    url(r"^main/$", views.main),
+    url(r"^login/$", views.login),
+    url(r"^showmain/$", views.showmain),
+    url(r"^quit/$", views.quit),
 
 ]
--- qianfeng_400/Django_learning/firstSite/project/myApp/views.py
+++ qianfeng_400/Django_learning/firstSite/project/myApp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import HttpRequest
 from .models import Grades
 from .models import Students
-
-
+from django.db.models import F, Q
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 # 定义了一个视图，也就是一个html页面,这是不用模板
 # def index(request):
 #     return HttpResponse("sunck is a good man !")
@@ -15,7 +16,11 @@ def index(request):
     return render(request, "myApp/index.html")
 
 
-def detail(request, num, num2):
+def detail1(request, num):
+    return HttpResponse("detail-%s" % num)
+
+
+def detail2(request, num, num2):
     return HttpResponse("detail-%s-%s" % (num, num2))
 
 
@@ -74,9 +79,6 @@ def gradesStudents(request, num):
     studentsList = grade.students_set.all()
     # 返回给模板
     return render(request, "myApp/students.html", {"students": studentsList})
-
-
-from django.db.models import F, Q
 
 
 # F对象的使用示例
@@ -151,3 +153,56 @@ def showresponse(request):
     print(res.status_code)
     # print(res.content-type)
     return HttpResponse("0000")
+
+
+def cookietest(request):
+    res = HttpResponse()
+
+    # F12中可以查看到Response响应头中的set-cookie的value是sunck=good
+    # cookie = res.set_cookie("sunck", "good")
+    # 有了上面的设置，以后的请求中cookie都带有此值，以下的语句取出cookie值
+    cookie = request.COOKIES
+    res.write("<h1>"+cookie["sunck"]+"</h1>")
+    return res
+
+
+
+def redirect1(request):
+    # 如果重定向的url前面没有/，就是在前一个url的基础上在后面直接加重定向的url，
+    # return HttpResponseRedirect("redirect2")就是形成/redirect1/redirect2,
+    # 如果加了/，就是写什么url就跳转到该url
+    # 此例子是为了从127.0.0.1:80000/sunck/redirct1，跳转到/sunck/redirect2,所以为了正常匹配，
+    # 应该写成/sunck/redirect2
+    # 使用HttpResponseRedirect重定向
+    # return HttpResponseRedirect("/sunck/redirect2/")
+    # 使用redirect重定向
+    return redirect("/sunck/redirect2/")
+
+
+def redirect2(request):
+    return HttpResponse("我是重定向的视图")
+
+
+def main(request):
+    username = request.session.get("name", default="游客")
+    print(username)
+    return render(request, "myApp/main.html", {"username": username})
+
+
+def login(request):
+    return render(request, "myApp/login.html")
+
+
+def showmain(request):
+    username = request.GET.get("uname")
+    request.session["name"] = username
+    request.session.set_expiry(10)
+    return redirect("/sunck/main")
+
+
+from django.contrib.auth import logout
+def quit(request):
+    # logout(request)
+    # request.session.clear()
+    request.session.flush()
+    return redirect("/sunck/main")
